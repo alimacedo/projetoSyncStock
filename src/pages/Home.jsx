d4165a1b056a1866f67d4682ac0ee5127ffc007a
alimacedo <alimacedoapolinario@gmail.com>
2025-07-14 13:24:17 -0300

    Ajustes na Home e inclusão de novas rotas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
       <div id="blocohome">
         <div className="boxhome">
           <img src="./src/images/gestao.png" className="imgboxhome" alt="Gestão de estoque" />
-          <h3><Link to="/gestao-estoque">Gestão de estoque</Link></h3>
+          <h3><Link to="/gestao-estoque">Gestão estoque</Link></h3>
         </div>
         <div className="boxhome">
           <img src="./src/images/projeto.png" className="imgboxhome" alt="Projetos" />
@@ -15,7 +15,19 @@ const Home = () => {
         <div className="boxhome">
           <img src="./src/images/fornecedor.png" className="imgboxhome" alt="Fornecedores" />
           <h3><Link to="/fornecedores">Fornecedores</Link></h3>
-        </div> 
+        </div>
+        <div className="boxhome">
+          <img src="./src/images/produto.png" className="imgboxhome" alt="Produtos" />
+          <h3><Link to="/produtos">Produtos</Link></h3>
+        </div>
+        <div className="boxhome">
+          <img src="./src/images/localarmazenamento.png" className="imgboxhome" alt="Armazenamento" />
+          <h3><Link to="/armazenamento">Armazenamento</Link></h3>
+        </div>
+        <div className="boxhome">
+          <img src="./src/images/notafiscal.png" className="imgboxhome" alt="notaFiscal" />
+          <h3><Link to="/nota-fiscal">Nota Fiscal</Link></h3>
+        </div>
       </div>
     </>
   );
